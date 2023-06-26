f9f9546cd353b5d3d447fdf054cc720bdbf29a55
steffydominic <116252088+steffydominic@users.noreply.github.com>
2023-06-26 19:54:46 +0530

    Add files via upload

---
+++ src/test/java/com/fssa/sa/sampleprogs/day17/solved/HelloTest.java
@@ -0,0 +1,19 @@
+package com.fssa.sa.sampleprogs.day17.solved;
+import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Sample test class
+ */
+public class HelloTest {
+	
+	/**
+	 * First test case
+	 */
+	@Test
+	public void helloTest() {
+		
+		Assertions.assertEquals("Hello World", "Hello World");
+	}
+}
---
+++ src/test/java/com/fssa/sa/sampleprogs/day18/solved/TestValidationUtil.java
@@ -0,0 +1,5 @@
+package com.fssa.sa.sampleprogs.day18.solved;
+
+public class TestValidationUtil {
+
+}
